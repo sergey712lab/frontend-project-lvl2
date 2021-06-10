19e14d765e80f0b5470d27a1c844e1ede9098a1a
Sergei <szhabich@gmail.com>
2021-06-10 15:31:31 +0300

    tests

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -1,20 +1,38 @@
-import fs from 'fs';
+import { fileURLToPath } from 'url';
 import path from 'path';
-import genDiff from '../files';
+import fs from 'fs';
+import genDiff from '../files/genDiff.js';
 
-const templates = [
-  [
-    'before.json',
-    'after.json',
-  ],
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
-const readFile = (fileName) => fs.readFileSync(getFixturePath(fileName), 'UTF-8');
+const file1Stylish = readFile('file1_after_stylish.txt');
+const file2Plain = readFile('file2_after_plain.txt');
+const file3Stylish = readFile('file3_after_stylish.txt');
+const file4Json = readFile('file4_after_json.txt');
 
-test.each(templates)('compare config, output format - json', (fileName1, fileName2) => {
-  const fullPath1 = getFixturePath(fileName1);
-  const fullPath2 = getFixturePath(fileName2);
-  const expectedValue = readFile('result-json.txt');
-  const actualValue = genDiff(fullPath1, fullPath2, 'json');
-  expect(actualValue).toEqual(expectedValue);
+const file1Json = getFixturePath('file1_before.json');
+const file2Json = getFixturePath('file2_before.json');
+const file2Yaml = getFixturePath('file2_before.yaml');
+
+test('stylish with json', () => {
+  const diff = genDiff(file1Json, file2Json);
+  expect(diff).toEqual(file1Stylish.trim());
+});
+
+test('stylish with json and yaml', () => {
+  const diff = genDiff(file1Json, file2Yaml);
+  expect(diff).toEqual(file3Stylish.trim());
+});
+
+test('plain', () => {
+  const diff = genDiff(file1Json, file2Json, 'plain');
+  expect(diff).toEqual(file2Plain.trim());
+});
+
+test('json', () => {
+  const diff = genDiff(file1Json, file2Json, 'json');
+  expect(diff).toEqual(file4Json.trim());
 });
