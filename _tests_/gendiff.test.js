35c6247adf1b335d0ac8d2a0b745d5db7647cc4a
Sergei <szhabich@gmail.com>
2021-06-10 15:57:53 +0300

    fix

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -1,38 +1,38 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-import fs from 'fs';
-import genDiff from '../files/genDiff.js';
+import { fileURLToPath } from 'url'
+import path from 'path'
+import fs from 'fs'
+import genDiff from '../files/genDiff.js'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename)
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8')
 
-const file1Stylish = readFile('file1_after_stylish.txt');
-const file2Plain = readFile('file2_after_plain.txt');
-const file3Stylish = readFile('file3_after_stylish.txt');
-const file4Json = readFile('file4_after_json.txt');
+const file1Stylish = readFile('file1_after_stylish.txt')
+const file2Plain = readFile('file2_after_plain.txt')
+const file3Stylish = readFile('file3_after_stylish.txt')
+const file4Json = readFile('file4_after_json.txt')
 
-const file1Json = getFixturePath('file1_before.json');
-const file2Json = getFixturePath('file2_before.json');
-const file2Yaml = getFixturePath('file2_before.yaml');
+const file1Json = getFixturePath('file1_before.json')
+const file2Json = getFixturePath('file2_before.json')
+const file2Yaml = getFixturePath('file2_before.yaml')
 
 test('stylish with json', () => {
-  const diff = genDiff(file1Json, file2Json);
-  expect(diff).toEqual(file1Stylish.trim());
-});
+  const diff = genDiff(file1Json, file2Json)
+  expect(diff).toEqual(file1Stylish.trim())
+})
 
 test('stylish with json and yaml', () => {
-  const diff = genDiff(file1Json, file2Yaml);
-  expect(diff).toEqual(file3Stylish.trim());
-});
+  const diff = genDiff(file1Json, file2Yaml)
+  expect(diff).toEqual(file3Stylish.trim())
+})
 
 test('plain', () => {
-  const diff = genDiff(file1Json, file2Json, 'plain');
-  expect(diff).toEqual(file2Plain.trim());
-});
+  const diff = genDiff(file1Json, file2Json, 'plain')
+  expect(diff).toEqual(file2Plain.trim())
+})
 
 test('json', () => {
-  const diff = genDiff(file1Json, file2Json, 'json');
-  expect(diff).toEqual(file4Json.trim());
-});
+  const diff = genDiff(file1Json, file2Json, 'json')
+  expect(diff).toEqual(file4Json.trim())
+})
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-import program from 'commander';
-import genDiff from '../files/genDiff.js';
+import program from 'commander'
+import genDiff from '../files/genDiff.js'
 // const program = new program();
 program
-  .version('0.1')  
-  .description('Compares two configuration files and shows a difference.')  
+  .version('0.1')
+  .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    const result = genDiff(filepath1, filepath2);
-    console.log(JSON.parse(result));
-  });
-  
-  program.parse();
+    const result = genDiff(filepath1, filepath2)
+    console.log(JSON.parse(result))
+  })
+
+program.parse()
