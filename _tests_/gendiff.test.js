e73b41610607a5fd78d3a3f4ff8cf1581fbbc0e6
Sergei <szhabich@gmail.com>
2021-06-12 14:55:13 +0300

    tests

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -7,13 +7,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-;
-const resultJson = readFile('result.txt');
+
+const file1Stylish = readFile('result.txt');
 
 const file1Json = getFixturePath('file1.json');
 const file2Json = getFixturePath('file2.json');
 
-test('json', () => {
-  const diff = genDiff(file1Json, file2Json, 'json');
-  expect(diff).toEqual(resultJson.trim());
+test('stylish with json', () => {
+  const diff = genDiff(file1Json, file2Json);
+  expect(diff).toEqual(file1Stylish.trim());
 });
